683df53c4bad13d8f1644403b660eb2b4409718d
Vasilkata <learningtocode14@gmail.com>
2025-10-22 18:26:43 +0300

    Added a too many args fix

---
+++ word-count.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[]) {
+	if (argc < 2) {
+        printf("Usage: %s <filename>\n", argv[0]);
+        return 1;
+    } else if(argc > 2) {
+		printf("Too many args!\n");
+		return -1;
+	}
+
+    FILE *file = fopen(argv[1], "r");
+
+    if (!file) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    int ch;
+    int in_word = 0;
+    int word_count = 0;
+
+    while ((ch = fgetc(file)) != EOF) {
+        if (isspace(ch))
+            in_word = 0;
+        else if (!in_word) {
+            in_word = 1;
+            word_count++;
+        }
+    }
+
+    fclose(file);
+
+    printf("Word count: %d\n", word_count);
+	return 0;
+}
